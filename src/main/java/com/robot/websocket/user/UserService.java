f62049420998a1f9e8023457325ac8b9b97c4c6e
AmimiHamza <amimi1hamza@gmail.com>
2024-04-30 03:59:54 +0100

    fix: set the email as the unique value

--- src/main/java/com/robot/websocket/user/UserService.java
+++ src/main/java/com/robot/websocket/user/UserService.java
@@ -5,28 +5,31 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.stereotype.Service;
 
+import java.sql.Date;
 import java.util.List;
+import java.sql.Date;
 
 @Service
 @AllArgsConstructor
 public class UserService {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
 
     public void saveUser(User user) {
         user.setStatus(Status.ONLINE);
-        repository.save(user);
+        userRepository.save(user);
     }
 
     public void disconnect(User user) {
-        var storedUser = repository.findById(user.getNickName()).orElse(null);
+        var storedUser = userRepository.findByEmail(user.getEmail());
         if (storedUser != null) {
             storedUser.setStatus(Status.OFFLINE);
-            repository.save(storedUser);
+            storedUser.setLastLogin(new java.util.Date());
+            userRepository.save(storedUser);
         }
     }
 
     public List<User> findAllUsers() {
-        return repository.findAll();
+        return userRepository.findAll();
     }
 }

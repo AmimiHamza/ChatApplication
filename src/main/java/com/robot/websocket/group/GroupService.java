63d7cb0d8da9462bb9163396893266cd1274c7ab
AmimiHamza <amimi1hamza@gmail.com>
2024-04-30 03:56:23 +0100

    fix:esit the groupid type and swich from nickname to email as the unique value of a user

--- src/main/java/com/robot/websocket/group/GroupService.java
+++ src/main/java/com/robot/websocket/group/GroupService.java
@@ -22,52 +22,53 @@ public class GroupService {
         group.setName(groupDTO.getName());
         group.setCreator(groupDTO.getCreator());
         group.setUsers(groupDTO.getUsers());
-        group.setId(groupDTO.getName() + groupDTO.getCreator() + System.currentTimeMillis());
+        //group.setId(groupDTO.getName() + groupDTO.getCreator() + System.currentTimeMillis());
+        group.setIsOffecial(false);
         final List<String> admins = new ArrayList<String>();
         admins.add(groupDTO.getCreator());
         group.setAdmins(admins);
         group.setCreatedDate(new java.sql.Date(System.currentTimeMillis()));
         for (String user : groupDTO.getUsers()) {
-            User user1 = UserRepository.findByNickName(user);
+            User user1 = UserRepository.findByEmail(user);
             user1.addGroup(group.getId());
             UserRepository.save(user1);
         }
         return groupRepository.save(group);
     }
-    public List<Group> findallgroupsofnickname(String nickname) {
-        return groupRepository.findAllByUser(nickname);
+    public List<Group> findallgroupsofemail(String email) {
+        return groupRepository.findAllByUser(email);
     }
-    public void deleteGroup(String groupId, String username) {
+    public void deleteGroup(Long groupId, String email) {
         Group group = groupRepository.findById(groupId).get();
-        if (group.getCreator().equals(username)) {
+        if (group.getCreator().equals(email)) {
             groupRepository.delete(group);
             for (String user : group.getUsers()) {
-                User user1 = UserRepository.findByNickName(user);
+                User user1 = UserRepository.findByEmail(user);
                 user1.removeGroup(groupId);
                 UserRepository.save(user1);
             }
         }
         throw new UnsupportedOperationException("Unimplemented method 'deleteGroup'");
     }
-    public Group findById(String groupId) {
+    public Group findById(Long groupId) {
         Group group=groupRepository.findById(groupId).get();
         return group;
     }
-    public List<User> findUsers(String groupId) {
+    public List<User> findUsers(Long groupId) {
         Group group = groupRepository.findById(groupId).get();
         List<User> users = new ArrayList<User>();
         for (String user : group.getUsers()) {
-            users.add(UserRepository.findByNickName(user));
+            users.add(UserRepository.findByEmail(user));
         }
         return users;
 
     }
-    public void addGroupMembers(String groupId, List<String> users) {
+    public void addGroupMembers(Long groupId, List<String> users) {
         Group group = groupRepository.findById(groupId).get();
         for (String user : users) {
             if (!group.getUsers().contains(user)){
             group.getUsers().add(user);
-            User user1 = UserRepository.findByNickName(user);
+            User user1 = UserRepository.findByEmail(user);
             user1.addGroup(groupId);
             UserRepository.save(user1);
         }
@@ -75,13 +76,13 @@ public class GroupService {
         groupRepository.save(group);
         
     }
-    public ResponseEntity<String>   removeGroupMember(String groupId, String memberId, String username) {
+    public ResponseEntity<String>   removeGroupMember(Long groupId, String memberId, String email) {
         Group group = groupRepository.findById(groupId).get();
-        if (!group.getCreator().equals(username)) {
+        if (!group.getCreator().equals(email)) {
             return ResponseEntity.badRequest().body("Creator cannot be removed");
         }else{
         group.getUsers().remove(memberId);
-        User user = UserRepository.findByNickName(memberId);
+        User user = UserRepository.findByEmail(memberId);
         user.removeGroup(groupId);
         UserRepository.save(user);
         groupRepository.save(group);
